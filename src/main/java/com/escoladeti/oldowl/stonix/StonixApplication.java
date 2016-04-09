7382763959fa3476d4e83911e29b2ed0efbf55b5
Tiago Couto <tiago@bearbone.com.br>
2016-04-09 10:17:21 -0300

    #0 First Commit

---
+++ src/main/java/com/escoladeti/oldowl/stonix/StonixApplication.java
@@ -0,0 +1,12 @@
+package com.escoladeti.oldowl.stonix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StonixApplication.class, args);
+    }
+}
---
+++ src/test/java/com/escoladeti/oldowl/stonix/StonixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.escoladeti.oldowl.stonix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StonixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
