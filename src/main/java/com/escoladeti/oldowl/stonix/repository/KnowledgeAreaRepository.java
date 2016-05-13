81bc2cb9bdec12e4544c61136007bc1f55d1eb47
FelipeBohnertPaetzold <felipe.paetzold@gmail.com>
2016-05-13 12:22:14 -0300

    #18 Create KnowledgeArea

---
+++ src/main/java/com/escoladeti/oldowl/stonix/repository/KnowledgeAreaRepository.java
@@ -0,0 +1,12 @@
+package com.escoladeti.oldowl.stonix.repository;
+
+import com.escoladeti.oldowl.stonix.model.KnowledgeArea;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Felipe on 13/05/2016.
+ */
+@Repository
+public interface KnowledgeAreaRepository extends JpaRepository<KnowledgeArea, String>{
+}
