91c84a903163e3a84d373517d05d8b1a0bd4d310
FelipeBohnertPaetzold <felipe.paetzold@gmail.com>
2016-05-28 22:19:14 -0300

    #7260 criacao de entidade Answer e liquibase

---
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/model/Answer.java
@@ -0,0 +1,35 @@
+package com.escoladeti.oldowl.stonix.forum.model;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by tdc on 01/05/16.
+ */
+@Entity
+public class Answer extends BasicForum {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Answer that = (Answer) o;
+        return Objects.equal(this.getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getDescription(), getId());
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", getId())
+                .add("description", getDescription())
+                .toString();
+    }
+}
