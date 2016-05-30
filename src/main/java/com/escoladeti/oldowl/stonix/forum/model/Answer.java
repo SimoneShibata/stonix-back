f9b526ab4198daea94ad940478ace1a202339fe5
FelipeBohnertPaetzold <felipe.paetzold@gmail.com>
2016-05-30 14:48:06 -0300

    #7323 Corrigido alteracao na linha de Answer

--- src/main/java/com/escoladeti/oldowl/stonix/forum/model/Answer.java
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/model/Answer.java
@@ -4,13 +4,9 @@ import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
-import java.util.List;
 
 /**
- * Created by Felipe on 01/05/16.
+ * Created by tdc on 01/05/16.
  */
 @Entity
 public class Answer extends BasicForum {
