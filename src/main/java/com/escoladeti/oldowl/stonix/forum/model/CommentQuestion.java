24553bcf79925d4d0bd080c485ee500b1c596275
FelipeBohnertPaetzold <felipe.paetzold@gmail.com>
2016-06-27 09:55:09 -0300

    #7681 integracao comentario e pergunta

--- src/main/java/com/escoladeti/oldowl/stonix/forum/model/CommentQuestion.java
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/model/CommentQuestion.java
@@ -1,25 +1,36 @@
 package com.escoladeti.oldowl.stonix.forum.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 /**
  * Created by Felipe on 30/05/16.
  */
-@MappedSuperclass
-@Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Comment extends Forum {
+@Entity
+public class CommentQuestion extends Comment {
+
+    @ManyToOne
+    @JsonIgnore
+    private Question question;
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Comment that = (Comment) o;
+        CommentQuestion that = (CommentQuestion) o;
         return Objects.equal(this.getId(), that.getId());
     }
 
---
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/repository/CommentQuestionRepository.java
@@ -0,0 +1,9 @@
+package com.escoladeti.oldowl.stonix.forum.repository;
+
+import com.escoladeti.oldowl.stonix.forum.model.CommentQuestion;
+
+/**
+ * Created by tdc on 09/04/16.
+ */
+public interface CommentQuestionRepository extends SuperRepository<CommentQuestion> {
+}
