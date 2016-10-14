31bae5cfcb875f3666d9a3b9204c6ee02a06be47
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2016-10-14 20:58:46 -0300

    #8718 criar atributo para tutorial

---
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/model/ClassRoom.java
@@ -0,0 +1,86 @@
+package com.escoladeti.oldowl.stonix.forum.model;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.List;
+
+/**
+ * Created by felipe on 13/10/16.
+ */
+@Entity
+public class ClassRoom extends SuperEntity {
+    @ManyToOne
+    private User teacher;
+
+    @ManyToMany
+    private List<User> students;
+
+    private String name;
+    private String description;
+
+    public User getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(User teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<User> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<User> students) {
+        this.students = students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClassRoom classRoom = (ClassRoom) o;
+
+        if (teacher != null ? !teacher.equals(classRoom.teacher) : classRoom.teacher != null) return false;
+        if (students != null ? !students.equals(classRoom.students) : classRoom.students != null) return false;
+        if (name != null ? !name.equals(classRoom.name) : classRoom.name != null) return false;
+        return description != null ? description.equals(classRoom.description) : classRoom.description == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = teacher != null ? teacher.hashCode() : 0;
+        result = 31 * result + (students != null ? students.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassRoom{" +
+                "teacher=" + teacher +
+                ", students=" + students +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/escoladeti/oldowl/stonix/forum/repository/ClassRoomRepository.java
@@ -0,0 +1,12 @@
+package com.escoladeti.oldowl.stonix.forum.repository;
+
+import com.escoladeti.oldowl.stonix.forum.model.ClassRoom;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by felipe on 13/10/16.
+ */
+@Repository
+public interface ClassRoomRepository extends SuperRepository<ClassRoom> {
+
+}
